c6b46533e88d02114f9bbe47fddc05fa7cd4a1ab
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-05-22 21:51:04 +0530

    Add Huffman compression-decompression

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from src.huffman import HuffmanCoding
+
+path = "../asset/sample.txt"
+
+h = HuffmanCoding(path)
+
+output_path = h.compress()
+print("Compressed file path: " + output_path)
+
+decom_path = h.decompress(output_path)
+print("Decompressed file path: " + decom_path)
